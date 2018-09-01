df29f50f91f6a6be54a016fdaea428c1e4588fbe
Zaven Muradyan <zaven@skepsi.me>
2018-08-31 21:28:48 -0700

    Fix mouse position bug.

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,7 +2,7 @@ extends RigidBody2D
 
 const ATTRACTOR_MIN_MOUSE_DIST = 75
 const ATTRACTOR_MIN_MOUSE_DIST_SQUARED = ATTRACTOR_MIN_MOUSE_DIST * ATTRACTOR_MIN_MOUSE_DIST
-const FORCE = 50
+const FORCE = 75
 
 onready var sprite = get_node("sprite")
 onready var rays = get_node("rays")
@@ -39,13 +39,14 @@ func _physics_process(delta):
 
 func _handle_mouse_motion(event):
 	# Flip sprite to the direction mouse is facing.
-	sprite.flip_h = global_position.x > event.position.x
+	var mouse_pos = get_global_mouse_position()
+	sprite.flip_h = global_position.x > mouse_pos.x
 	
 	# Determine target attractors.
 	var attractors = get_tree().get_nodes_in_group("attractors")
 	var nearby = []
 	for attractor in attractors:
-		if event.position.distance_squared_to(attractor.global_position) < ATTRACTOR_MIN_MOUSE_DIST_SQUARED:
+		if mouse_pos.distance_squared_to(attractor.global_position) < ATTRACTOR_MIN_MOUSE_DIST_SQUARED:
 			nearby.append(attractor)
 
 	nearby_attractors = nearby
